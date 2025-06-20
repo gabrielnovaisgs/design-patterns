3848e53823c2a2a9a9ff9d20a28f97459f2161a3
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2025-06-20 17:16:28 -0300

    Initialize Python project structure with essential files and singleton pattern implementation

---
+++ singleton.py
@@ -0,0 +1,33 @@
+
+class Configuration:
+    instance: "Configuration | None" = None
+    configs: dict[str, str]
+    def __init__(self):
+        print(f'instance created!')
+
+
+    @classmethod
+    def get_instance(cls) -> "Configuration":
+        
+        if(not cls.instance):
+            cls.instance = Configuration()
+        return cls.instance
+
+    
+    def save_configuration(self, configs: dict[str, str]):
+        self.configs = configs
+        print("configurations saved!")
+    
+    def show_configs(self):
+        print(f'System configurations: {[f'{config}: {self.configs[config]}' for config in self.configs]}')
+
+
+obj1 = Configuration.get_instance()
+obj2 = Configuration.get_instance()
+
+obj1.save_configuration({
+    "dark": 'true',
+    'zoom': "5x"
+})
+
+obj2.show_configs()
